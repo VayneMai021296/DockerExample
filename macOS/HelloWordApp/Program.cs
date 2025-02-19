f2385d9fd3dca5b73d3d2bcf9df31bd20a880089
Vayne <manmuoi021204@gmail.com>
2025-02-19 14:43:32 +0700

    init: initialize docker example project

---
+++ macOS/HelloWordApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("What is your name?: ");
+string name = Console.ReadLine() ?? "";
+if(name != ""){
+    Console.WriteLine("Nice to meet you, " + name + "!");
+}
+else{
+    Console.WriteLine("Nice to meet you!");
+}   
+
+Console.ReadLine();
+
+
